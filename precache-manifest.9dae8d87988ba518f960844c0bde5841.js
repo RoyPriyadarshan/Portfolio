d1c7b72fba0fb86a69778522c6709dcd9b78bcf2
RoyPriyadarshan <skullboydanger1@gmail.com>
2020-06-25 23:41:07 +0530

    Updates

---
+++ precache-manifest.9dae8d87988ba518f960844c0bde5841.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ea969912fef4458b0fc527f7691a89",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "71df2f596f0f6f90413a",
+    "url": "/portfolio/static/css/2.21ed39d3.chunk.css"
+  },
+  {
+    "revision": "a43cfab8b60f0eccaa85",
+    "url": "/portfolio/static/css/main.c11e874a.chunk.css"
+  },
+  {
+    "revision": "71df2f596f0f6f90413a",
+    "url": "/portfolio/static/js/2.8501f78e.chunk.js"
+  },
+  {
+    "revision": "b7b068e7a9ff7c79ac98ce106091f1d8",
+    "url": "/portfolio/static/js/2.8501f78e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a43cfab8b60f0eccaa85",
+    "url": "/portfolio/static/js/main.bc58b8b1.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "9cac8be1e061e684cb449f40e36323b7",
+    "url": "/portfolio/static/media/Intro.9cac8be1.jpg"
+  },
+  {
+    "revision": "b111245e6d7ee069e6657c5c19573854",
+    "url": "/portfolio/static/media/aboutMe.b111245e.jpg"
+  },
+  {
+    "revision": "541b9714b3a43c39a9a90cc6546b70fd",
+    "url": "/portfolio/static/media/contact.541b9714.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bee8373e95706eeb1066ad0210771c32",
+    "url": "/portfolio/static/media/hotelManagement.bee8373e.jpg"
+  },
+  {
+    "revision": "3a2b2700a951536a5b7ff9492a49f375",
+    "url": "/portfolio/static/media/mathGame.3a2b2700.jpg"
+  },
+  {
+    "revision": "64790c81344a7751769e4a27c64b3213",
+    "url": "/portfolio/static/media/mathWebsite.64790c81.jpg"
+  },
+  {
+    "revision": "d19d0e2c7097f992756c22537f2c7370",
+    "url": "/portfolio/static/media/profile.d19d0e2c.jpg"
+  },
+  {
+    "revision": "bc0165d5331219c2cbdb8c3a479cf635",
+    "url": "/portfolio/static/media/project.bc0165d5.jpg"
+  },
+  {
+    "revision": "5f060507252be5ad72086a69af9b63cb",
+    "url": "/portfolio/static/media/resourceAllocator.5f060507.jpg"
+  },
+  {
+    "revision": "48d00bc4a054a2854c44113bc471b967",
+    "url": "/portfolio/static/media/restrauntWebsite.48d00bc4.jpg"
+  },
+  {
+    "revision": "a68344ddd21531f20b1314ad164fe69d",
+    "url": "/portfolio/static/media/skills.a68344dd.jpg"
+  }
+]);
